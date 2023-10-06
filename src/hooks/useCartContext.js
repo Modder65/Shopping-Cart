d1177b01d06362bfd2466ef9b1af7874e449f84e
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-10-06 18:26:11 -0400

    Removed unnecessary error check in the useCartContext file

--- src/hooks/useCartContext.js
+++ src/hooks/useCartContext.js
@@ -3,8 +3,5 @@ import { CartContext } from "../components/CartProvider";
 
 export const useCartContext = () => {
   const context = useContext(CartContext);
-  if (!context) {
-    throw new Error("useCart must be used within a CartProvider");
-  }
   return context;
 };
